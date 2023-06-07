cc9f523edb4722f245ecd2227c4ee0b5e2be40d7
Weaver Yung <weaveryu@student.21-school.ru>
2023-06-07 21:23:13 +0000

    Update copy_files.sh

--- src/copy_files.sh
+++ src/copy_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp ./s21_cat weaveryu@10.10.10.3:~
-scp ./s21_grep weaveryu@10.10.10.3~
+scp ./s21_cat weaveryu@10.10.10.3:~/s21_cat
+scp ./s21_grep weaveryu@10.10.10.3~/s21_grep
 
 ssh weaveryu@10.10.10.3 'sudo cp ~/s21_cat /usr/local/bin/s21_cat'
 ssh weaveryu@10.10.10.3 'sudo cp ~/s21_grep /usr/local/bin/s21_grep'
