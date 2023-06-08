0c00b6db04e8a701dc474b4c1fb8a5c16dc0485c
Weaver Yung <weaveryu@student.21-school.ru>
2023-06-08 11:56:16 +0000

    Update notify.sh

--- src/notify.sh
+++ src/notify.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TELEGRAM_USER_ID=pipeline_buddy_bot
-
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
